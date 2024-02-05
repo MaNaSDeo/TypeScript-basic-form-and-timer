bba59a4dca551a15ea4b21b6a9312034ee71f148
MaNaSDeo <rmnslogin@gmail.com>
2024-02-06 00:40:55 +0530

    useReducer

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,12 +1,18 @@
-import { type ReactNode, createContext, useContext } from "react";
+import { type ReactNode, createContext, useContext, useReducer } from "react";
 
 type Timer = {
   name: string;
   duration: number;
 };
+
 type TimersState = {
   isRunning: boolean;
   timers: Timer[];
+};
+
+const initialState: TimersState = {
+  isRunning: true,
+  timers: [],
 };
 
 type TimersContextValue = TimersState & {
@@ -31,20 +37,63 @@ type TimersContextProviderProps = {
   children: ReactNode;
 };
 
+type StartTimerAction = {
+    type: 'START_TIMER'
+}
+type StopTimerAction = {
+    type: 'STOP_TIMER'
+}
+type AddTimerAction = {
+    type: 'ADD_TIMER';
+    payload: Timer
+}
+
+type Action = StartTimerAction | StopTimerAction| AddTimerAction;
+
+function timersReducer(state: TimersState, action: Action): TimersState {
+  if (action.type === "START_TIMER") {
+    return {
+      ...state,
+      isRunning: true,
+    };
+  }
+  if (action.type === 'STOP_TIMER' {
+    return {
+      ...state,
+      isRunning: false,
+    };
+  }
+  if (action.type === "ADD_TIMER") {
+    return {
+      ...state,
+      timers: [
+        ...state.timers,
+        {
+            name: action.payload.name,
+            duration: action.payload.duration,
+        }
+      ]
+    };
+  }
+  return state;
+}
+
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: true,
+    timers: timersState.timers,
+    isRunning: timersState.isRunning,
     addTimer(timerData) {
-      //...
+      dispatch({ type: "ADD_TIMER", payload: timerData });
     },
     startTimers() {
-      //...
+      dispatch({ type: "START_TIMER" });
     },
     stopTimers() {
-      //...
+      dispatch({ type: "STOP_TIMER" });
     },
   };
   return (
