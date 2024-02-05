2bacbf34991b66f04cf0cc1c5f5c403aff452cd4
MaNaSDeo <rmnslogin@gmail.com>
2024-02-05 11:14:31 +0530

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
+  );
 }
 
 export default App;
